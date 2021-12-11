ed3463c50e74ccec509811a147094248e3f50747
Andrey Yaromenok <ayaromenok@gmail.com>
2021-12-11 12:12:36 +0200

    #2 update log in config

--- config/esp32diy.h
+++ config/esp32diy.h
@@ -178,9 +178,10 @@
 #define HAL_BOARD_STORAGE_DIRECTORY "/SDCARD/APM/STORAGE"
 #define HAL_OS_POSIX_IO 1
 
-#define HAL_LOGGING_FILESYSTEM_ENABLE 1
+//#define HAL_LOGGING_FILESYSTEM_ENABLE 1
 
 // this becomes the default value for the ardupilot param LOG_BACKEND_TYPE, which most ppl want to be 1, for log-to-flash
 // setting to 2 means log-over-mavlink to a companion computer etc.
-//#define HAL_LOGGING_BACKENDS_DEFAULT 1
+//#define HAL_LOGGING_ENABLE 1
+#define HAL_LOGGING_BACKENDS_DEFAULT 1
 
